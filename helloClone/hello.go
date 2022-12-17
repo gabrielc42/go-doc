33ff94112432778133d8e96f7c201ad09359cb57
gabrielc42 <clark.gabe97@gmail.com>
2022-12-17 16:12:06 -0600

    unable to run binaries

---
+++ helloClone/hello.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"example/user/hello/morestrings"
+	"fmt"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func main() {
+	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
+}
