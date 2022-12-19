63b3c29c0695a5a9c212677adbc0e1d842298a18
gabrielc42 <clark.gabe97@gmail.com>
2022-12-18 19:15:38 -0600

    data access, problem with env vars error 1045

---
+++ data-access/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/go-sql-driver/mysql"
+)
+
+var db *sql.DB
+
+func main() {
+	// Capture connection properties.
+	cfg := mysql.Config{
+		User:                 os.Getenv("DBUSER"),
+		Passwd:               os.Getenv("DBPASS"),
+		Net:                  "tcp",
+		Addr:                 "127.0.0.1:3306",
+		DBName:               "recordings",
+		AllowNativePasswords: true,
+	}
+	// Get a database handle.
+	var err error
+	db, err = sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+	fmt.Println("Connected!")
+}
---
+++ data-access/recordings.sql
@@ -0,0 +1,20 @@
+drop database if exists recordings;
+create database recordings;
+
+drop table if exists album;
+create table album (
+	id int primary key auto_increment,
+    title varchar(128) not null,
+    artist varchar(255) not null,
+    price decimal(5,2) not null
+);
+
+insert into album
+  (title, artist, price)
+values
+  ('Blue Train', 'John Coltrane', 56.99),
+  ('Giant Steps', 'John Coltrane', 63.99),
+  ('Jeru', 'Gerry Mulligan', 17.99),
+  ('Sarah Vaughan', 'Sarah Vaughan', 34.98);
+  
+select * from album;
