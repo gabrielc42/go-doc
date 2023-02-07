49a578bb4effedf9fd08a744bcbe23eee98fad89
gabrielc42 <clark.gabe97@gmail.com>
2023-02-06 22:20:51 -0600

    go finalizers

--- finalizers/finalizers.go
+++ finalizers/finalizers.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"math/rand"
 	"runtime"
+	"runtime/debug"
 	"strconv"
 	"time"
 )
@@ -13,6 +14,35 @@ type Foo struct {
 }
 
 func main() {
+	debug.SetGCPercent(-1)
+	var ms runtime.MemStats
+	runtime.ReadMemStats(&ms)
+
+	fmt.Printf("Allocation: %f Mb, Number of allocation: %d\n", float32(ms.HeapAlloc)/float32(1024*1024), ms.HeapObjects)
+
+	for i := 0; i < 10; i++ {
+		f := NewFoo(i)
+		_ = fmt.Sprintf("%d", f.a)
+	}
+
+	runtime.ReadMemStats(&ms)
+	fmt.Printf("Allocation: %f Mb, Number of allocation: %d\n", float32(ms.HeapAlloc)/float32(1024*1024), ms.HeapObjects)
+
+	runtime.GC()
+	time.Sleep(time.Second)
+
+	runtime.ReadMemStats(&ms)
+	fmt.Printf("Allocation: %f Mb, Number of allocation: %d\n", float32(ms.HeapAlloc)/float32(1024*1024), ms.HeapObjects)
+
+	runtime.GC()
+	time.Sleep(time.Second)
+
+	runtime.ReadMemStats(&ms)
+	fmt.Printf("Allocation: %f Mb, Number of allocation: %d\n", float32(ms.HeapAlloc)/float32(1024*1024), ms.HeapObjects)
+
+	runtime.GC()
+	time.Sleep(time.Second)
+
 	for i := 0; i < 3; i++ {
 		f := NewFoo(i)
 		println(f.a)
@@ -21,6 +51,7 @@ func main() {
 	time.Sleep(1 * time.Second)
 }
 
+//go:noinline
 func NewFoo(i int) *Foo {
 	f := &Foo{a: rand.Intn(50)}
 	runtime.SetFinalizer(f, func(f *Foo) {
